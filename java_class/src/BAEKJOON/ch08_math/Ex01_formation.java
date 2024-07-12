9d022922c94f4430b472d2a7cb31f1290c32c53a
Hyosang56 <jhsaa01@naver.com>
2024-07-12 17:14:31 +0900

    수학

--- java_class/src/BAEKJOON/ch08_math/Ex01_formation.java
+++ java_class/src/BAEKJOON/ch08_math/Ex01_formation.java
@@ -1,4 +1,36 @@
 package BAEKJOON.ch08_math;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Ex01_formation {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String n = st.nextToken();
+        int sum = 0;
+        int exponent = n.length()-1;
+        int b = Integer.parseInt(st.nextToken());
+        for (int i = 0; i <n.length() ; i++) {
+            int a = con(n.charAt(i));
+            sum += (int)(a*(Math.pow(b,exponent)));
+            exponent --;
+        }
+        System.out.println(sum);
+    }
+    public static int con(char word){
+        if(word >= 'A' && word <='Z'){
+            return 10 + (word -'A');
+        } else {
+            return Character.getNumericValue(word);
+        }
+    }
 }
+
+
+
+
+
