2619547a769592896ede582674fa4e7712c9cd38
Hyosang56 <jhsaa01@naver.com>
2024-07-22 13:33:48 +0900

    수학

--- java_class/src/BAEKJOON/ch08_math/Ex04_center.java
+++ java_class/src/BAEKJOON/ch08_math/Ex04_center.java
@@ -1,7 +1,21 @@
 package BAEKJOON.ch08_math;
 
-public class Ex04_center {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Ex04_center {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int a = 2;
+        int b = 1;
+        double num = 4;
+        for (int i = 0; i <n ; i++) {
+            a += b;
+            num = Math.pow(a, 2);
+            b *= 2;
+        }
+        System.out.println((int)num);
     }
 }
