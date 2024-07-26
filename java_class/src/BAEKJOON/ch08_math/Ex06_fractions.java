0a96f0af6517d265e21bcc2ce802711908e530d2
Hyosang56 <jhsaa01@naver.com>
2024-07-26 15:56:19 +0900

    수학(분수)

---
+++ java_class/src/BAEKJOON/ch08_math/Ex06_fractions.java
@@ -0,0 +1,19 @@
+package BAEKJOON.ch08_math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex06_fractions {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
+        int a =1;
+        int b =1;
+        String str = a+"/"+b;
+        for (int i = 1; i <x ; i++) {
+
+        }
+        System.out.println(str);
+    }
+}
